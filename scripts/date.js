73aea98c824d04828d583563f4678b1483f21212
Anton Sanakoev <totum42@gmail.com>
2025-05-10 11:53:01 +0400

    Base layout

---
+++ scripts/date.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentyear.innerHTML = today.getFullYear();
+lastModified.innerHTML = 'Last Update: ' + document.lastModified;
